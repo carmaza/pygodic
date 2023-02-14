4a9e3e94b20b6fa833b7ba7694e39220c0100ea0
Cristóbal Armaza <ca455@cornell.edu>
2023-02-13 23:27:32 -0500

    Rename parameter f func

--- tests/finite_difference.py
+++ tests/finite_difference.py
@@ -15,7 +15,7 @@ class FirstDeriv:
     """
 
     @staticmethod
-    def second_order(f, x):
+    def second_order(func, x):
         r"""
         Approximate the first derivative using second-order finite difference
         on a grid of $N$ arbitrarily spaced points labeled $j = 0, \hdots, N-1$.
@@ -42,30 +42,30 @@ class FirstDeriv:
         The derivative of the function on each grid point.
 
         """
-        n = f.size
+        n = func.size
         dfdx = np.zeros(n)
 
         delta_0 = x[1] - x[0]
         delta_1 = x[2] - x[1]
 
-        dfdx[0] = (-(2. * delta_0 + delta_1) * f[0] / delta_0 -
-                   delta_0 * f[2] / delta_1) / (delta_0 + delta_1) + (
-                       delta_0 + delta_1) * f[1] / delta_0 / delta_1
+        dfdx[0] = (-(2. * delta_0 + delta_1) * func[0] / delta_0 -
+                   delta_0 * func[2] / delta_1) / (delta_0 + delta_1) + (
+                       delta_0 + delta_1) * func[1] / delta_0 / delta_1
 
         for j in range(1, n - 1):
             delta_j = x[j + 1] - x[j]
             delta_jm1 = x[j] - x[j - 1]
 
-            dfdx[j] = (delta_jm1 * f[j + 1] / delta_j - delta_j * f[j - 1] /
-                       delta_jm1) / (delta_j + delta_jm1) + (
-                           delta_j - delta_jm1) * f[j] / delta_j / delta_jm1
+            dfdx[j] = (delta_jm1 * func[j + 1] / delta_j - delta_j *
+                       func[j - 1] / delta_jm1) / (delta_j + delta_jm1) + (
+                           delta_j - delta_jm1) * func[j] / delta_j / delta_jm1
 
         delta_nm2 = x[n - 1] - x[n - 2]
         delta_nm3 = x[n - 2] - x[n - 3]
 
         dfdx[n - 1] = (
-            (2. * delta_nm2 + delta_nm3) * f[n - 1] / delta_nm2 +
-            delta_nm2 * f[n - 3] / delta_nm3) / (delta_nm2 + delta_nm3) - (
-                delta_nm2 + delta_nm3) * f[n - 2] / delta_nm2 / delta_nm3
+            (2. * delta_nm2 + delta_nm3) * func[n - 1] / delta_nm2 +
+            delta_nm2 * func[n - 3] / delta_nm3) / (delta_nm2 + delta_nm3) - (
+                delta_nm2 + delta_nm3) * func[n - 2] / delta_nm2 / delta_nm3
 
         return dfdx
