fb05c25680665f8795edbe0ce3b704919f3882cf
Cristóbal Armaza <ca455@cornell.edu>
2023-02-22 14:58:18 -0500

    Clean up class - Fix docstring - Sort properties alphabetically - Add property typing - Clean up doc so it looks nice using sphinx

--- pygodic/interpolants/density_from_potential.py
+++ pygodic/interpolants/density_from_potential.py
@@ -1,11 +1,12 @@
 # Distributed under the MIT License.
 # See LICENSE for details.
 """
-Defines `interpolants.DensityFromPotential`.
+Defines class `LogRhoVsLogPsi`.
 
 """
 
 import numpy as np
+import numpy.typing as npt
 
 from scipy.interpolate import UnivariateSpline
 
@@ -14,37 +15,35 @@ class LogRhoVsLogPsi(UnivariateSpline):
     """
     Interpolant for the log-log mass density vs relative potential relationship.
 
+    Parameters
+    ----------
+
+    model : obj
+        The spherical model used to construct the interpolant. Must be a
+        SphericalModel instance.
+
+    r_bounds : tuple
+        The bounds of the radial grid on which to evaluate the fields.
+
+    pts_rad : int
+        The target number of points of the radial grid.
+
+    k : int (optional, default: 3)
+        The degree of the smoothing spline. Must be one of ``[1, 2, 3, 4, 5]``.
+        `k = 3` is a cubic spline.
+
     Notes
     -----
 
-    This class inherits from `scipy.interpolate.UnivariateSpline`. See the SciPy
-    documentation for details.
+    This class inherits from ``scipy.interpolate.UnivariateSpline``. See the
+    SciPy documentation for details.
 
     """
 
     def __init__(self, model, r_bounds, pts_rad, k=3):
-        """
-        Parameters
-        ----------
-
-        model : obj
-            The spherical model used to construct the interpolant. Must be a
-            SphericalModel instance.
-
-        r_bounds : tuple
-            The bounds of the radial grid on which to evaluate the fields.
-
-        pts_rad : int
-            The target number of points of the radial grid.
-
-        k : int (optional, default: 3)
-            The degree of the smoothing spline. Must be one of [1, 2, 3, 4, 5].
-            k = 3 is a cubic spline.
-
-        """
         self._radial_grid = np.linspace(r_bounds[0], r_bounds[1], pts_rad)
 
-        # Radial profiles are decreasing so we need to flip arrays to interpolate.
+        # Radial profiles are decreasing so we flip arrays to interpolate.
         self._logpsi = np.log10(
             np.flip(model.relative_potential(self._radial_grid)))
         self._logrho = np.log10(np.flip(model.mass_density(self._radial_grid)))
@@ -56,15 +55,7 @@ class LogRhoVsLogPsi(UnivariateSpline):
                          check_finite=True)
 
     @property
-    def radial_grid(self):
-        """
-        The radial grid on which to evaluate the fields.
-
-        """
-        return self._radial_grid
-
-    @property
-    def logpsi(self):
+    def logpsi(self) -> npt.NDArray:
         """
         The logarithm of the relative potential.
 
@@ -72,9 +63,17 @@ class LogRhoVsLogPsi(UnivariateSpline):
         return self._logpsi
 
     @property
-    def logrho(self):
+    def logrho(self) -> npt.NDArray:
         """
         The logarithm of the mass density.
 
         """
         return self._logrho
+
+    @property
+    def radial_grid(self) -> npt.NDArray:
+        """
+        The radial grid on which to evaluate the fields.
+
+        """
+        return self._radial_grid
